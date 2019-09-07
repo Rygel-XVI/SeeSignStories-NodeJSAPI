f1837b95831edf7e59e65370896fad2889608850
Rygel-XVI <micha232@umn.edu>
2019-09-07 13:11:55 -0700

    removed unused code added unique validation to eTags and cascade delete for channel

--- src/models/video.js
+++ src/models/video.js
@@ -1,27 +1,14 @@
-// 'use strict';
-// module.exports = (sequelize, DataTypes) => {
-//   const Video = sequelize.define('Video', {
-//     hostSite: DataTypes.STRING,
-//     videoId: DataTypes.STRING,
-//     eTag: DataTypes.STRING
-//   }, {});
-//   Video.associate = (models) => {
-//     Video.belongsTo(models.Channel, {
-//       foreignKey: 'channelId',
-//       onDelete: 'CASCADE'
-//     })
-//     // associations can be defined here
-//   };
-//   return Video;
-// };
-
-
 const video = (sequelize, DataTypes) => {
+
   const Video = sequelize.define('video', {
     hostSite: DataTypes.STRING,
     videoId: DataTypes.STRING,
-    eTag: DataTypes.STRING
+    eTag: {
+      type: DataTypes.STRING,
+      unique: true,
+    }
   });
+
   Video.associate = models => {
     Video.belongsTo(models.Channel);
   };
