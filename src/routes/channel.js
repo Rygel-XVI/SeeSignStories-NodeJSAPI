c428b245d17f72b1164b6ed5f3d97cb5c90c270d
Rygel-XVI <micha232@umn.edu>
2019-09-07 11:05:45 -0700

    added notes on how the controller flow will go in each route

--- src/routes/channel.js
+++ src/routes/channel.js
@@ -10,4 +10,12 @@ router.get('/:channelId', (req, res) => {
     return res.send(req.context.models.channel[req.params.channelId]);
 });
 
+// check to see if channel eTag has changed
+    // if it has changed => check send request via video eTag for each video and update them and the channel eTag
+        // if it has not changed serve the client with current db info
+
+
+// if this is too many queries and i'm hitting my limit w/ youtube then only update at specific intervals or add another api key
+
+
 export default router;
--- src/routes/video.js
+++ src/routes/video.js
@@ -6,8 +6,17 @@ router.get('/', (req, res) => {
     return res.send(Object.values(req.context.models.video));
 });
 
-router.get('/:channelId', (req, res) => {
+router.get('/:videoId', (req, res) => {
     return res.send(req.context.models.video[req.params.videoId]);
 });
+
+routes I need
+
+// get with etag match to see if it has changed
+//     if it has changed update that entry in the db via post
+            // if video cannot be found delete from db
+
+        // then once checked if etag has changed serve the client
+
 
 export default router;
